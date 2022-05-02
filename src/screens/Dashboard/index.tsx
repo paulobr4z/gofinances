5e543d43e0610f49e648d7648c52a34557cf1eff
paulobr4z <paulobrazaraujo@gmail.com>
2022-05-02 14:25:33 -0300

    Chapter II - Utilizando Ícones

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -2,11 +2,31 @@ import React from 'react';
 
 import {
   Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
 } from './styles';
 
 export function Dashboard() {
   return (
     <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo 
+              source={{ uri: 'https://github.com/paulobr4z.png' }} 
+            />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Paulo Braz</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
